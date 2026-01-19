59a27748751294c713efc3d97ee995a918eede28
kabir-create <tanishcmpunk@gmail.com>
2026-01-19 10:34:02 +0530

    reinitialised commit

--- src/models/Bill.js
+++ src/models/Bill.js
@@ -2,18 +2,22 @@ const { pool } = require("../config/db");
 
 exports.createBill = async ({ tableNo, totalAmount, splitAmount, groupCode }) => {
   const { rows } = await pool.query(
-    `INSERT INTO bills (table_no, total_amount, split_amount, group_code, status)
-     VALUES ($1,$2,$3,$4,'OPEN')
-     RETURNING *`,
+    `
+    INSERT INTO bills (table_no, total_amount, split_amount, group_code, status)
+    VALUES ($1, $2, $3, $4, 'OPEN')
+    RETURNING *
+    `,
     [tableNo, totalAmount, splitAmount, groupCode]
   );
+
   return rows[0];
 };
 
 exports.getBillByGroupCode = async (groupCode) => {
   const { rows } = await pool.query(
-    `SELECT * FROM bills WHERE group_code=$1`,
+    `SELECT * FROM bills WHERE group_code = $1`,
     [groupCode]
   );
+
   return rows[0];
 };
